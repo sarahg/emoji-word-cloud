688e7db28cff68fc71a6288500f60a8f86132b11
Sarah German <sarah@advomatic.com>
2016-10-02 16:57:18 -0500

    Refactor emoji script as a class.

--- emoji_word_cloud/emoji_word_cloud.py
+++ emoji_word_cloud/emoji_word_cloud.py
@@ -1,52 +1,44 @@
-from sys import argv
 from emoji import UNICODE_EMOJI
-import json
-
-script, input_file = argv
 
 
-max_font_size = 200
+MAX_FONT_SIZE = 200
 
 
-def is_emoji(s):
-    """ Check if a character is an emoji. """
-    count = 0
-    for emoji in UNICODE_EMOJI:
-        count += s.count(emoji)
-        if count > 1:
-            return False
-    return bool(count)
+class EmojiCloud(text):
+
+    def __init__(text):
+
+        # Get a count of each unique emoji.
+        unique_characters = list(set(text))
+        emojis = [] 
+        for character in unique_characters:
+            if is_emoji(character):
+                emoji = {'character': character, 'count': text.count(character)}
+                emojis.append(emoji)
 
 
-def get_emoji_weight(count, highest_count):
-    """ Assign a font sized proportional to total occurances. """
-    font_size = (count * max_font_size) / highest_count
-    return int(font_size)
+        # Get the most frequent emoji.
+        top_emoji = max(emojis, key=lambda x:x['count'])
 
 
-# Read the given text file.
-text = open(input_file).read()
+        # Set weights.
+        for emoji in emojis:
+            emoji['weight'] = get_emoji_weight(emoji['count'], top_emoji['count'])
+
+        return emojis
 
 
-# Get a count of each unique emoji.
-unique_characters = list(set(text))
-emojis = []
- 
-for character in unique_characters:
-    if is_emoji(character):
-        emoji = {'character': character, 'count': text.count(character)}
-        emojis.append(emoji)
+    def is_emoji(s):
+        """ Check if a character is an emoji. """
+        count = 0
+        for emoji in UNICODE_EMOJI:
+            count += s.count(emoji)
+            if count > 1:
+                return False
+        return bool(count)
 
 
-# Get the most frequent emoji.
-top_emoji = max(emojis, key=lambda x:x['count'])
-
-
-# Set weights.
-for emoji in emojis:
-    emoji['weight'] = get_emoji_weight(emoji['count'], top_emoji['count'])
-    
-
-
-# Print a JSON object containing emojis, weights and counts.
-print (json.dumps(emojis))
+    def get_emoji_weight(count, highest_count):
+        """ Assign a font sized proportional to total occurances. """
+        font_size = (count * MAX_FONT_SIZE) / highest_count
+        return int(font_size)
