29038bb2d6185ef621e1f5e862e396cb0b3a57a5
Sarah German <sarah@advomatic.com>
2016-10-02 16:57:31 -0500

    Create and add routes for Flask app.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import emoji_word_cloud
+
+app = Flask(__name__)
+
+@app.route('/')
+def wordIn():
+    return render_template('wordCloud.html')
+
+@app.route('wordUp', methods=['POST'])
+def wordUp():
+    text = request.form['emoji']
+    emojis = emoji_word_cloud.EmojiCloud(text)
+    
+    return json.dumps({'status':'OK','weighted_emojis':emojis})
+
+if __name__ == "__main__":
+    app.run()
